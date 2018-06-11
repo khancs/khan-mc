ba67ea28c97398eefa3a2697a3333527d3ac836b
khancs <khanacademycs+github@gmail.com>
2018-06-10 22:44:49 -0400

    Create advancements.js to include all advancements

---
+++ assets/advancements.js
@@ -0,0 +1,5 @@
+(function(global) {
+  global.assets.advancements = {};
+  (["advancements/adventure", "advancements/end", "advancements/husbandry",
+    "advancements/nether", "advancements/recipes", "advancements/story"]).forEach(include);
+})(window);
