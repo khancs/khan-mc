3edb3d2cb53688083c679e2f2e16de9e3a9e42f6
khancs <khanacademycs+github@gmail.com>
2018-06-10 23:03:25 -0400

    Add base URL to include function

--- assets/include.js
+++ assets/include.js
@@ -3,9 +3,12 @@
   global.include = function(name) {
     var el = document.createElement("script");
     el.setAttribute("async", false);
-    el.setAttribute("src", name + ".js");
+    el.setAttribute("src", global.include.baseURL + name + ".js");
     document.head.insertBefore(el, document.head.firstChild);
   };
+  var thisScript = document.getElementsByTagName("script");
+  thisScript = thisScript[thisScript.length - 1].getAttribute("src");
+  global.include.baseURL = thisScript.substring(0, thisScript.lastIndexOf("/") + 1);
   (["advancements", "blockstates", "lang", "loot_tables", "models",
     "recipes", "structures", "texts", "textures"]).forEach(include);
 })(window);
